2f9943c51d527a01a0fad97d187ee669ff11f366
insomn14 <barirehatta@gmail.com>
2020-10-27 09:00:55 +0700

    add solver

--- b01lers/Crypto/Train of Thought/solver.py
+++ b01lers/Crypto/Train of Thought/solver.py
@@ -1,15 +1,11 @@
 import Levenshtein as L
-from itertools import permutations
-from string import ascii_lowercase as lw
+import string
 
-f = open('trainofthought.txt').read().strip().split(' ')
 
-idx = []
+f = open("trainofthought.txt", "r").read().strip().split(' ')
+flag = ''
 for i in range(len(f)-1):
-	idx.append(L.distance(f[i], f[(i+1)%len(f)]))
-
-print(idx)
-
-lw = '\x00' + lw
-flag = ''.join(lw[j%len(lw)] for j in idx)
+	dist = L.distance(f[i], f[i+1])
+	chrs = string.ascii_lowercase[dist-1]
+	flag += chrs		
 print(flag)
